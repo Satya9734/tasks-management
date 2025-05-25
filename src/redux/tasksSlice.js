a373e1f3411a687e37672e7474dd86c2e4b927f8
satya mallick <satyamallick973@gmail.com>
2025-05-25 17:19:14 +0530

    fixing data after add

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -8,8 +8,7 @@ const response=await axios.post("https://task-back-rosy.vercel.app/dashbord/addt
     withCredentials:true
 });
 const data=await response.data;
-console.log("data after add:",data);
-return data.tasks;
+return data.data;
 }
 catch(err){
     const message =
